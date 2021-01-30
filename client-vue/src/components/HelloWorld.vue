9e3a125c264eadc4c860b36ea0c107cccbdb522e
Toni <aamengual@cifpfbmoll.eu>
2021-01-30 18:05:29 +0100

    falla get despres de post en mateixa funcio?

--- client-vue/src/components/HelloWorld.vue
+++ client-vue/src/components/HelloWorld.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <!-- Cheat Sheet: Template String -->
     <h1>{{ msg }}</h1>
-    <button v-on:click="addPost" class="btn btn-primary">Add Post (with random contents)</button>
+    <button v-on:click="addPost" class="btn btn-primary">
+      Add Post (with random contents)
+    </button>
     <table class="table">
       <thead>
         <tr>
@@ -28,82 +30,85 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg:String
-  }, 
+    msg: String,
+  },
   data: () => {
     return {
-      posts: []
-    }
+      posts: [],
+    };
   },
   computed: {
-    postsUpdated: function() {
-      return this.posts
-    }
+    postsUpdated: function () {
+      return this.posts;
+    },
   },
   mounted() {
     // Carrega posts
     this.getPosts();
   },
   methods: {
-    getPosts: function() {
-      fetch('http://localhost:3000/posts', { method: 'GET'})
-      .then(response => response.json())
-      .then( (data) => {
-        this.posts = data
-      })
-    }, 
-    deletePost: function(id) {
-        //Delete from API
-        fetch(`http://localhost:3000/posts/${id}` , { method: 'DELETE'})
-        .then(response => response.json())
+    getPosts: function () {
+      fetch("http://localhost:3000/posts", { method: "GET" })
+        .then((response) => response.json())
+        .then((data) => {
+          this.posts = data;
+        });
+    },
+    deletePost: function (id) {
+      //Delete from API
+      fetch(`http://localhost:3000/posts/${id}`, { method: "DELETE" })
+        .then((response) => response.json())
         .then(() => {
           //Delete from posts
           this.posts.forEach((element, key) => {
             if (element.id == id) {
               // Elimina dels posts també
-              this.posts.splice(key,1);
+              this.posts.splice(key, 1);
               return;
-          }
-        }); 
-      }) 
+            }
+          });
+        });
     },
 
-    addPost: function() {
-      let url = "https://my-json-server.typicode.com/classicoman2/fakeRESTserver/posts"
+    addPost: function () {
+      let url =
+        "https://my-json-server.typicode.com/classicoman2/fakeRESTserver/posts";
       // Genera post aleatoriament
-      let id = Math.floor(Math.random() * 3)+1;
+      let id = Math.floor(Math.random() * 3) + 1;
 
-            // Resolem la promesa amb then
-      creaPost()
-      .then( (response) => response.json())
-      .then( (json) => {
-         console.info("Post Creat:", json)
-         // Update llistat
-         this.getPosts()
-      })
-      .catch( (error) => console.error("Ha passat aquest error:", error))
-
-      async function creaPost() {
-        // Captura post remot
-        const responseGet = await fetch(url + "/" + id)
-        const json = await responseGet.json()
-        console.warn(json)
-        // Envia POST
-        const responsePost = await fetch('http://localhost:3000/posts', {
-            method: 'POST',
-            body: JSON.stringify({ title: json.title, contents: json.contents }),
-            headers: {
-              "Content-type": "application/json; charset=UTF-8"
-            }
+      // Resolem la promesa amb then
+      creaPostiActualitza()
+        .then((response) => response.json())
+        .then((json) => {
+          console.info("Posts actualitzats:", json);
         })
-        return responsePost
-    }
-  }
+        .catch((error) => console.error("Ha passat aquest error:", error));
 
-  }
-}
+      async function creaPostiActualitza() {
+        // Captura post remot
+        const responseGet = await fetch(url + "/" + id);
+        const json = await responseGet.json();
+        console.warn(json);
+        // Envia POST
+        const responsePost = await fetch("http://localhost:3000/posts", {
+          method: "POST",
+          body: JSON.stringify({ title: json.title, contents: json.contents }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        });
+        const jsonResposta = await responsePost.json();
+        console.info("Post Creat:", jsonResposta);
+        //Actualitza
+
+        // Update llistat
+        return fetch('http://localhost:3000/posts');
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
